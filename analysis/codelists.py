bc8966b7996b233f7b0ece9eb4360d1593dd5f89
Alex Walker <alex.walker@thedatalab.org>
2021-09-28 22:40:05 +0100

    add codelists

---
+++ analysis/codelists.py
@@ -0,0 +1,38 @@
+from cohortextractor import codelist, codelist_from_csv, combine_codelists
+
+covid_codes = codelist_from_csv(
+    "codelists/opensafely-covid-identification.csv",
+    system="icd10",
+    column="icd10_code",
+)
+covid_primary_care_positive_test = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-positive-test.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+covid_primary_care_code = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-clinical-code.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+covid_primary_care_sequalae = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-sequelae.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+any_primary_care_code = combine_codelists(
+    covid_primary_care_code,
+    covid_primary_care_positive_test,
+    covid_primary_care_sequalae,
+)
+ethnicity_codes = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_6",
+)
+sick_notes_codes = codelist_from_csv(
+    "codelists/user-alex-walker-sick-notes-ctv3.csv",
+    system="ctv3",
+    column="code",
+)
