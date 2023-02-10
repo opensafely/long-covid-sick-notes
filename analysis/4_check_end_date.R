b59680f34097a4bb1d7a98c27b6f21ecb75037b4
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-02-10 14:46:28 +0000

    Fixed typo

--- analysis/4_check_end_date.R
+++ analysis/4_check_end_date.R
@@ -12,6 +12,7 @@ library(tools)
 library(here)
 library(fs)
 library(RColorBrewer)
+library(lubridate)
 
 # For running locally only #
 # setwd("C:/Users/aschaffer/OneDrive - Nexus365/Documents/GitHub/long-covid-sick-notes")
@@ -28,9 +29,9 @@ read <- function(data){
   read_dta(here::here("output","cohorts",data)) %>%
     mutate(
         sick_note_end_date = as.Date(sick_note_end_date, format = "%Y-%m-%d"),
-        index_date = as.Date(sick_note_end_date, format = "%Y-%m-%d"),
+        indexdate = as.Date(indexdate, format = "%Y-%m-%d"),
         end_month  = floor_date(as.Date(sick_note_end_date), unit="month"),
-        index_month =  floor_date(as.Date(index_date), unit="month")
+        index_month =  floor_date(as.Date(indexdate), unit="month")
           ) %>% 
   dplyr::select(c(patient_id, end_month, index_month, sick_note)) 
 
@@ -56,19 +57,19 @@ tab <- function(data) {
 ####### Save files ##########
 
 covid20_tab <- tab(covid20)
-write_csv(covid20_tab, here::here("output", "released_outputs", "end_date_covid20.csv"))
+write_csv(covid20_tab, here::here("output", "tabfig", "end_date_covid20.csv"))
 
 covid21_tab <- tab(covid21)
-write_csv(covid21_tab, here::here("output", "released_outputs", "end_date_covid21.csv"))
+write_csv(covid21_tab, here::here("output", "tabfig", "end_date_covid21.csv"))
 
 gen19_tab <- tab(gen19)
-write_csv(gen19_tab, here::here("output", "released_outputs", "end_date_gen19.csv"))
+write_csv(gen19_tab, here::here("output", "tabfig", "end_date_gen19.csv"))
 
 gen20_tab <- tab(gen20)
-write_csv(gen20_tab, here::here("output", "released_outputs", "end_date_gen20.csv"))
+write_csv(gen20_tab, here::here("output", "tabfig", "end_date_gen20.csv"))
 
 gen21_tab <- tab(gen21)
-write_csv(gen21_tab, here::here("output", "released_outputs", "end_date_gen21.csv"))
+write_csv(gen21_tab, here::here("output", "tabfig", "end_date_gen21.csv"))
 
 
 
