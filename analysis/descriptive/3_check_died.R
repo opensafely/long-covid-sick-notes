16a8de37983f2f4be16254296f1f3e056253f538
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-05-18 10:25:36 +0100

    Fix date formatting

--- analysis/descriptive/3_check_died.R
+++ analysis/descriptive/3_check_died.R
@@ -27,26 +27,52 @@ dir_create(here::here("output", "tabfig"), showWarnings = FALSE, recurse = TRUE)
 ##### Read in data for each cohort #####
 
 covid20 <- read_dta(here::here("output", "cohorts", "combined_covid_2020_general_2019.dta")) %>%
-  subset(case == 1)
+  subset(case == 1) %>%
+  mutate(indexdate = as.Date(indexdate, format = "%Y-%m-%d"),
+         died_date_ons = as.Date(died_date_ons,format = "%Y-%m-%d"),
+         sick_note_end_date = as.Date(sick_note_end_date, format = "%Y-%m-%d"))
 
 covidhosp20 <- read_dta(here::here("output", "cohorts", "combined_covid_2020_pneumonia.dta")) %>%
-  subset(case == 1)
+  subset(case == 1) %>%
+  mutate(indexdate = as.Date(indexdate, format = "%Y-%m-%d"),
+         died_date_ons = as.Date(died_date_ons,format = "%Y-%m-%d"),
+         sick_note_end_date = as.Date(sick_note_end_date, format = "%Y-%m-%d"))
 
 covid21 <- read_dta(here::here("output", "cohorts", "combined_covid_2021_general_2019.dta")) %>%
-  subset(case == 1)
+  subset(case == 1)%>%
+  mutate(indexdate = as.Date(indexdate, format = "%Y-%m-%d"),
+         died_date_ons = as.Date(died_date_ons,format = "%Y-%m-%d"),
+         sick_note_end_date = as.Date(sick_note_end_date, format = "%Y-%m-%d"))
 
 covidhosp21 <- read_dta(here::here("output", "cohorts", "combined_covid_2021_pneumonia.dta")) %>%
-  subset(case == 1)
+  subset(case == 1) %>%
+  mutate(indexdate = as.Date(indexdate, format = "%Y-%m-%d"),
+         died_date_ons = as.Date(died_date_ons,format = "%Y-%m-%d"),
+         sick_note_end_date = as.Date(sick_note_end_date, format = "%Y-%m-%d"))
 
 pneumo19 <- read_dta(here::here("output", "cohorts", "combined_covid_2020_pneumonia.dta")) %>%
-  subset(case ==0)
+  subset(case ==0)%>%
+  mutate(indexdate = as.Date(indexdate, format = "%Y-%m-%d"),
+         died_date_ons = as.Date(died_date_ons,format = "%Y-%m-%d"),
+         sick_note_end_date = as.Date(sick_note_end_date, format = "%Y-%m-%d"))
 
 gen19 <- read_dta(here::here("output", "cohorts", "combined_covid_2020_general_2019.dta")) %>%
-  subset(case ==0)
+  subset(case ==0)%>%
+  mutate(indexdate = as.Date(indexdate, format = "%Y-%m-%d"),
+         died_date_ons = as.Date(died_date_ons,format = "%Y-%m-%d"),
+         sick_note_end_date = as.Date(sick_note_end_date, format = "%Y-%m-%d"))
+
 gen20 <- read_dta(here::here("output", "cohorts", "combined_covid_general_2020.dta")) %>%
-  subset(case ==0)
+  subset(case ==0)%>%
+  mutate(indexdate = as.Date(indexdate, format = "%Y-%m-%d"),
+         died_date_ons = as.Date(died_date_ons,format = "%Y-%m-%d"),
+         sick_note_end_date = as.Date(sick_note_end_date, format = "%Y-%m-%d"))
+
 gen21 <- read_dta(here::here("output", "cohorts", "combined_covid_general_2021.dta")) %>%
-  subset(case ==0)
+  subset(case ==0)%>%
+  mutate(indexdate = as.Date(indexdate, format = "%Y-%m-%d"),
+         died_date_ons = as.Date(died_date_ons,format = "%Y-%m-%d"),
+         sick_note_end_date = as.Date(sick_note_end_date, format = "%Y-%m-%d"))
 
 
 ###### Number who died #####
