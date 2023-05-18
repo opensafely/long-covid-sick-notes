89178727d1328dd897b3309bbecf61058629ca6c
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-05-18 11:37:53 +0100

    Changes due to Stata unavailability

--- analysis/descriptive/3_check_died.R
+++ analysis/descriptive/3_check_died.R
@@ -76,20 +76,20 @@ gen21 <- read_dta(here::here("output", "cohorts", "cohort_rates_matched_2021.dta
 ###### Number who died #####
 died <- function(dat, cohort, enddate){
   
-  enddate = as.Date(enddate)
+  enddate <- as.Date(enddate)
   
   dat2 <- dat %>% 
-    mutate(died = if_else(!is.na(died_date_ons) & 
+    mutate(died_censor = if_else(!is.na(died_date_ons) & 
                             died_date_ons == sick_note_end_date, 1, 0, 0),
+           died_any = if_else(!is.na(died_date_ons) &
+                                died_date_ons >= indexdate &
+                                died_date_ons <= enddate, 1, 0, 0),
            time = as.integer(sick_note_end_date - indexdate),
-           cohort = cohort,
-           indexmissing = if_else(is.na(indexdate), 1, 0, 0),
-           endmissing = if_else(is.na(sick_note_end_date), 1, 0, 0)) %>%
+           cohort = cohort) %>%
     group_by(cohort) %>%
     summarise(n = n(),
-              n_died = sum(died),
-              n_indexmiss = sum(indexmissing),
-              n_endmiss = sum(endmissing),
+              n_died_any = sum(died_any),
+              n_died_censor = sum(died_censor),
               median_time = median(time, na.rm = TRUE),
               q25 = quantile(time, 0.25, na.rm = TRUE), 
               q75 = quantile(time, 0.75, na.rm = TRUE))
@@ -106,15 +106,21 @@ all <- rbind(
               died(covidhosp20, "COVID hospitalised 2020", "2020-11-30"),
               died(covidhosp21, "COVID hospitalised 2021", "2021-11-30"),
               died(pneumo19, "Pneumonia 2019", "2019-11-30"),
-              died(gen20, "General pop 2020","2020-11-30"),
+              died(gen20, "General pop 2020", "2020-11-30"),
               died(gen21, "General pop 2021", "2021-11-30"),
               died(gen19, "General pop 2019", "2019-11-30")
               ) %>%
   mutate(n = case_when(n > 5 ~ n),
          n = round(n / 7) * 7,
-         n_died = case_when(n_died > 5 ~ n_died),
-         n_died = round(n_died / 7) * 7,
-         pcent = n_died / n * 100)
+         
+         n_died_any = case_when(n_died_any > 5 ~ n_died_any),
+         n_died_any = round(n_died_any / 7) * 7,
+         
+         n_died_censor = case_when(n_died_censor > 5 ~ n_died_censor),
+         n_died_censor = round(n_died_censor / 7) * 7,
+         
+         pcent_any = n_died_any / n * 100,
+         pcent_censor = n_died_censor / n *100)
   
 
 write.csv(all, here::here("output", "tabfig", "check_died.csv"))
