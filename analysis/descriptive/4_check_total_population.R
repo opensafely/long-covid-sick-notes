86804defd179274748d90c84869c4dc377305390
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-07-19 16:14:13 +0100

    Check total population

---
+++ analysis/descriptive/4_check_total_population.R
@@ -0,0 +1,51 @@
+
+
+################################################################
+# This script:
+# - Creates frequency table of COVID diagnosis type
+#   (SGSS test, primary care, hospital diagnosis)
+#
+# Author: Andrea Schaffer
+################################################################
+
+library(haven)
+library(tidyverse)
+library(reshape2)
+library(tools)
+library(here)
+library(fs)
+library(RColorBrewer)
+
+# For running locally only #
+# setwd("C:/Users/aschaffer/OneDrive - Nexus365/Documents/GitHub/long-covid-sick-notes")
+# getwd()
+
+
+## Create directories if needed
+dir_create(here::here("output", "tabfig"), showWarnings = FALSE, recurse = TRUE)
+dir_create(here::here("output", "cohorts"), showWarnings = FALSE, recurse = TRUE)
+
+
+##### Read in data for each cohort #####
+pop20 <- read_csv(here::here("output", "cohorts", "input_general_match_vars_2020-02-01.csv.gz")) 
+pop21 <- read_csv(here::here("output", "cohorts", "input_general_match_vars_2021-02-01.csv.gz")) 
+pop22 <- read_csv(here::here("output", "cohorts", "input_general_match_vars_2022-02-01.csv.gz")) 
+
+
+pop20_cnt <- pop20 %>%
+  summarise(tot = n()) %>%
+  mutate(year = "2020")
+
+pop21_cnt <- pop21 %>%
+  summarise(tot = n()) %>%
+  mutate(year = "2021")
+
+pop22_cnt <- pop22 %>%
+  summarise(tot = n()) %>%
+  mutate(year = "2022")
+
+
+population_all <- rbind(pop20_cnt, pop21_cnt, pop22_cnt)
+
+write.csv(population_all, here::here("output","tabfig","population_by_year.csv"),
+          row.names = FALSE)
